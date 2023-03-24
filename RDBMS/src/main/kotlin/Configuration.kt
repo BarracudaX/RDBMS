10c53721a52e765d562914d8fd74e9f058737947
Arslan <a.kalaitsidis@msensis.com>
2023-03-24 12:15:04 +0200

    Add init script for mysql and transaction support.

--- RDBMS/src/main/kotlin/Configuration.kt
+++ RDBMS/src/main/kotlin/Configuration.kt
@@ -1,9 +1,11 @@
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.transaction.PlatformTransactionManager
 import javax.sql.DataSource
 
 @Configuration
@@ -13,7 +15,11 @@ open class Configuration {
     fun dataSource() : DataSource =
         EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
+            .addScript("classpath:script.sql")
             .build()
+
+    @Bean
+    fun transactionManager(dataSource: DataSource) : PlatformTransactionManager = DataSourceTransactionManager(dataSource)
 
     @Bean
     fun namedJdbcTemplate(dataSource: DataSource) : NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(dataSource)
--- RDBMS/src/test/kotlin/AbstractRDBMSTest.kt
+++ RDBMS/src/test/kotlin/AbstractRDBMSTest.kt
@@ -2,8 +2,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.transaction.annotation.Transactional
 
 @SpringJUnitConfig(Configuration::class)
-abstract class AbstractRDBMSTest @Autowired constructor(private val jdbcTemplate: NamedParameterJdbcTemplate){
+@Transactional
+abstract class AbstractRDBMSTest{
+
+    protected lateinit var jdbcTemplate: NamedParameterJdbcTemplate
 
 }
